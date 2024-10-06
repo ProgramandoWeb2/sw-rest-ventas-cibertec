c3dac948c73c2405f77a46c4f65f29eb57446a4e
ProgramandoWeb2 <daplicacionesmoviles1@gmail.com>
2024-10-05 21:11:35 -0500

    model completo

---
+++ src/main/java/pe/edu/cibertec/sw_rest_ventas_cibertec/model/Employee.java
@@ -0,0 +1,35 @@
+package pe.edu.cibertec.sw_rest_ventas_cibertec.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer employeeid;
+    private String lastname;
+    private String firstname;
+    private String title;
+    private String titleofcourtesy;
+    private Date birthdate;
+    private Date hiredate;
+    private String address;
+    private String city;
+    private String region;
+    private String postalcode;
+    private String country;
+    private String homephone;
+    private String extension;
+    private String notes;
+    private Integer reportsto;
+    private String photopath;
+    private Double salary;
+
+}
---
+++ src/main/java/pe/edu/cibertec/sw_rest_ventas_cibertec/model/pk/ProductOrderId.java
@@ -0,0 +1,14 @@
+package pe.edu.cibertec.sw_rest_ventas_cibertec.model.pk;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter @Setter
+@Embeddable
+public class ProductOrderId implements Serializable {
+    private Integer orderid;
+    private Integer productid;
+}
